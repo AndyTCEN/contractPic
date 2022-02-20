7adf376bfc9df79cc08d743cdfa220e20437e5fa
AndyTCEN <andy77122@hotmail.com>
2022-02-20 14:04:15 +0800

    test

---
+++ precache-manifest.ba280d419fb92372dd89d2aa600ed2c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "809e787196867c14590d",
+    "url": "/static/js/main.809e7871.chunk.js"
+  },
+  {
+    "revision": "514243a1b0acbef97783",
+    "url": "/static/css/1.b9198a3b.chunk.css"
+  },
+  {
+    "revision": "514243a1b0acbef97783",
+    "url": "/static/js/1.514243a1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1237054e0cf723d88b69fe5e31242e15",
+    "url": "/index.html"
+  }
+];
