84ff322a36d8ce7dc8f8d63365d998f5d30be36a
AndyTCEN <andy77122@hotmail.com>
2022-02-20 18:02:59 +0800

    eth change

---
+++ precache-manifest.1515abddb8f98698bf7523129b91e4a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "712899fa28c53ccedb62",
+    "url": "/contractPic/static/js/main.712899fa.chunk.js"
+  },
+  {
+    "revision": "3c6f6fd8197e9f7ae529",
+    "url": "/contractPic/static/css/1.4221b26a.chunk.css"
+  },
+  {
+    "revision": "3c6f6fd8197e9f7ae529",
+    "url": "/contractPic/static/js/1.3c6f6fd8.chunk.js"
+  },
+  {
+    "revision": "e30995d9d33a3436da2e",
+    "url": "/contractPic/static/js/runtime~main.e30995d9.js"
+  },
+  {
+    "revision": "b73283761bc2572340ea9da290c1cae7",
+    "url": "/contractPic/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contractPic/precache-manifest.47470be3e40635f0b8eccf2dcc7d944a.js"
+  "/contractPic/precache-manifest.1515abddb8f98698bf7523129b91e4a8.js"
 );
 
 workbox.clientsClaim();
